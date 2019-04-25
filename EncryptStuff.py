2b7ac9b81e02cd678c77d8699a79f379ba78d3bd
Skickar <40251293+skickar@users.noreply.github.com>
2019-04-24 22:40:10 -0700

    bid b

--- EncryptStuff.py
+++ EncryptStuff.py
@@ -1,1 +1,12 @@
+import pyAesCrypt
 
+def encryptDat(i):
+    # encryption/decryption buffer size - 64K
+    bufferSize = 64 * 1024
+    password = "foopassword"
+    # encrypt
+    pyAesCrypt.encryptFile("data" + (str(i)) + ".aes", "data" + (str(i+1)) + ".aes", password, bufferSize)
+for i in range (1, 10):
+    for i in range (1, 10):
+      encryptDat(i)
+print("Done!")
